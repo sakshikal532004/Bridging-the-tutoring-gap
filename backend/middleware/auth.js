2e3bc869307176a94c368feaf63b30f26f9d09bb
Sakshi Sachin Kalyankar <sakshikalyankar123@gmail.com>
2025-09-26 02:10:20 +0530

    Create auth.js

---
+++ backend/middleware/auth.js
@@ -0,0 +1,17 @@
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const auth = (req, res, next) => {
+  const token = req.header("Authorization")?.split(" ")[1];
+  if (!token) return res.status(401).json({ msg: "No token" });
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch {
+    res.status(401).json({ msg: "Invalid token" });
+  }
+};
